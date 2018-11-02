8e5945b6fe8207a5d20867ea6391dd8a41c17648
Zac Clark <zandoh9@gmail.com>
2018-11-01 22:07:56 -0400

    Add handling for no items equipped

--- src/components/characterItems/CharacterItems.js
+++ src/components/characterItems/CharacterItems.js
@@ -8,60 +8,116 @@ class CharacterItems extends React.Component {
       <div className="characterItemsContainer characterColumn">
         <h2 className="itemsHeader">Items</h2>
         <div className="characterItemRow">
-          <div className="characterSlot">Head: </div>
-          <CharacterItem item={this.props.items.head} />
+          {this.props.items.head && (
+            <React.Fragment>
+              <div className="characterSlot">Head: </div>
+              <CharacterItem item={this.props.items.head} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Neck: </div>
-          <CharacterItem item={this.props.items.neck} />
+          {this.props.items.neck && (
+            <React.Fragment>
+              <div className="characterSlot">Neck: </div>
+              <CharacterItem item={this.props.items.neck} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Shoulder: </div>
-          <CharacterItem item={this.props.items.shoulder} />
+          {this.props.items.shoulder && (
+            <React.Fragment>
+              <div className="characterSlot">Shoulder: </div>
+              <CharacterItem item={this.props.items.shoulder} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Back: </div>
-          <CharacterItem item={this.props.items.back} />
+          {this.props.items.back && (
+            <React.Fragment>
+              <div className="characterSlot">Back: </div>
+              <CharacterItem item={this.props.items.back} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Chest: </div>
-          <CharacterItem item={this.props.items.chest} />
+          {this.props.items.chest && (
+            <React.Fragment>
+              <div className="characterSlot">Chest: </div>
+              <CharacterItem item={this.props.items.chest} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Wrist: </div>
-          <CharacterItem item={this.props.items.wrist} />
+          {this.props.items.wrist && (
+            <React.Fragment>
+              <div className="characterSlot">Wrist: </div>
+              <CharacterItem item={this.props.items.wrist} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Hands: </div>
-          <CharacterItem item={this.props.items.hands} />
+          {this.props.items.hands && (
+            <React.Fragment>
+              <div className="characterSlot">Hands: </div>
+              <CharacterItem item={this.props.items.hands} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Waist: </div>
-          <CharacterItem item={this.props.items.waist} />
+          {this.props.items.waist && (
+            <React.Fragment>
+              <div className="characterSlot">Waist: </div>
+              <CharacterItem item={this.props.items.waist} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Legs: </div>
-          <CharacterItem item={this.props.items.legs} />
+          {this.props.items.legs && (
+            <React.Fragment>
+              <div className="characterSlot">Legs: </div>
+              <CharacterItem item={this.props.items.legs} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Feet: </div>
-          <CharacterItem item={this.props.items.feet} />
+          {this.props.items.feet && (
+            <React.Fragment>
+              <div className="characterSlot">Feet: </div>
+              <CharacterItem item={this.props.items.feet} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Finger 1: </div>
-          <CharacterItem item={this.props.items.finger1} />
+          {this.props.items.finger1 && (
+            <React.Fragment>
+              <div className="characterSlot">Finger 1: </div>
+              <CharacterItem item={this.props.items.finger1} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Finger 2: </div>
-          <CharacterItem item={this.props.items.finger2} />
+          {this.props.items.finger2 && (
+            <React.Fragment>
+              <div className="characterSlot">Finger 2: </div>
+              <CharacterItem item={this.props.items.finger2} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Trinket 1: </div>
-          <CharacterItem item={this.props.items.trinket1} />
+          {this.props.items.trinket1 && (
+            <React.Fragment>
+              <div className="characterSlot">Trinket 1: </div>
+              <CharacterItem item={this.props.items.trinket1} />
+            </React.Fragment>
+          )}
         </div>
         <div className="characterItemRow">
-          <div className="characterSlot">Trinket 2: </div>
-          <CharacterItem item={this.props.items.trinket2} />
+          {this.props.items.trinket2 && (
+            <React.Fragment>
+              <div className="characterSlot">Trinket 2: </div>
+              <CharacterItem item={this.props.items.trinket2} />
+            </React.Fragment>
+          )}
         </div>
       </div>
     );
